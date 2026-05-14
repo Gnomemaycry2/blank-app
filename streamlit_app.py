32dab75b2f643240b38809ece4873e88086cd61a
Gnomemaycry2 <ulad.khatskevich@gmail.com>
2026-05-14 21:15:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if manual_angle != 0.0:
     st.session_state.guess_made = True
     
     st.header("Calculated Angle")
-    st.metric("Vertical Angle to Point", f"{vertical_angle_deg:.0f}°")
+    #st.metric("Vertical Angle to Point", f"{vertical_angle_deg:.0f}°")
     
     difference = abs(manual_angle - vertical_angle_deg)
     
