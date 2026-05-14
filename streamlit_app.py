e9ae5dfa21a98c4eed1c79a08eb8eedc830d0a67
Gnomemaycry2 <ulad.khatskevich@gmail.com>
2026-05-14 22:56:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,8 @@ with col3:
 
 # Calculate vertical angle
 vertical_angle_rad = math.atan(altitude / distance)
-vertical_angle_deg = math.degrees(vertical_angle_rad) or 0 
+vertical_angle_deg = math.degrees(vertical_angle_rad) 
+vertical_angle_deg = 0 if my_value is None else vertical_angle_deg 
 
 # Manual input and comparison
 #st.header("Manual Input & Comparison")
